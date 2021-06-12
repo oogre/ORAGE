9e7df702ce30450c3bf5bc61e7538ee8d4eed388
oogre <vincent@ogre.be>
2021-06-12 15:59:54 +0200

    VictorMono/

--- include/Resources.h
+++ include/Resources.h
@@ -3,11 +3,11 @@
 
 //#define RES_MY_RES			CINDER_RESOURCE( ../resources/, image_name.png, 128, IMAGE )
 
-#define VICTOR_MONO_REGULAR         CINDER_RESOURCE( ../resources/, VictorMono/VictorMono-Regular.ttf, 128, TTF_FONT )
-#define VICTOR_MONO_REGULAR_OBLIQUE CINDER_RESOURCE( ../resources/, VictorMono/VictorMono-Oblique.ttf, 128, TTF_FONT )
-#define VICTOR_MONO_THIN            CINDER_RESOURCE( ../resources/, VictorMono/VictorMono-Thin.ttf, 128, TTF_FONT )
-#define VICTOR_MONO_THIN_OBLIQUE    CINDER_RESOURCE( ../resources/, VictorMono/VictorMono-ThinOblique.ttf, 128, TTF_FONT )
-#define VICTOR_MONO_BOLD            CINDER_RESOURCE( ../resources/, VictorMono/VictorMono-Bold.ttf, 128, TTF_FONT )
-#define VICTOR_MONO_BOLD_OBLIQUE    CINDER_RESOURCE( ../resources/, VictorMono/VictorMono-BoldOblique.ttf, 128, TTF_FONT )
+#define VICTOR_MONO_REGULAR         CINDER_RESOURCE( ../resources/VictorMono/, VictorMono-Regular.ttf, 128, TTF_FONT )
+#define VICTOR_MONO_REGULAR_OBLIQUE CINDER_RESOURCE( ../resources/VictorMono/, VictorMono-Oblique.ttf, 128, TTF_FONT )
+#define VICTOR_MONO_THIN            CINDER_RESOURCE( ../resources/VictorMono/, VictorMono-Thin.ttf, 128, TTF_FONT )
+#define VICTOR_MONO_THIN_OBLIQUE    CINDER_RESOURCE( ../resources/VictorMono/, VictorMono-ThinOblique.ttf, 128, TTF_FONT )
+#define VICTOR_MONO_BOLD            CINDER_RESOURCE( ../resources/VictorMono/, VictorMono-Bold.ttf, 128, TTF_FONT )
+#define VICTOR_MONO_BOLD_OBLIQUE    CINDER_RESOURCE( ../resources/VictorMono/, VictorMono-BoldOblique.ttf, 128, TTF_FONT )
 
 
